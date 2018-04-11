286882c4d6d3f5276c68647fc9bfcc1146623808
nmw923 <38275275+nmw923@users.noreply.github.com>
2018-04-11 17:19:21 +0900

    Update main.c

--- main.c
+++ main.c
@@ -18,5 +18,24 @@ int sub(int a, int b){
 }
 
 int main(){
-
+int a,b;
+char c;
+ 
+for(int i =0; i <5; i++){
+  scanf("%d %c %d",&a,c,&b);
+ if(c == '+'){
+  printf("%d + %d = %d\n",a,b,add(a,b));
+ }
+  else if(c == '-'){
+  printf("%d - %d = %d\n",a,b,sub(a,b));
+ }
+  else if(c == '*'){
+  printf("%d * %d = %d\n",a,b,mul(a,b));
+ }
+ else if(c == '/'){
+  printf("%d / %d = %d\n",a,b,div(a,b));
+ }
+ 
+}
+ 
 }
